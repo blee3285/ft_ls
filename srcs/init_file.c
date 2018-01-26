f08dc94ac6b268c3c4e709031715557795e6cbf4
blee3285 <benlee3285@gmail.com>
2018-01-25 19:17:37 -0800

    starting on permissions

--- srcs/init_file.c
+++ srcs/init_file.c
@@ -6,11 +6,26 @@
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/24 17:50:56 by blee              #+#    #+#             */
-/*   Updated: 2018/01/24 19:18:08 by blee             ###   ########.fr       */
+/*   Updated: 2018/01/25 19:17:12 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
+
+char	*get_perm(mode_t fmode)
+{
+	char	*new;
+
+	new = ft_strnew(10);
+	if (fmode & S_IRUSR)
+		new[0] = 'r';
+	else
+		new[0] = '-';
+	if (fmode & S_IWUSR)
+		new[1] = 'w';
+	else
+		new[1] = '-';-
+}
 
 t_file	*new_file(char *str)
 {
