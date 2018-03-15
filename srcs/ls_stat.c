78cae6928d945c5a08c3431f202f9523c4eaeeeb
Benson Lee <blee@e1z2r7p30.42.us.org>
2018-03-14 20:58:59 -0700

    still planning

--- srcs/ls_stat.c
+++ srcs/ls_stat.c
@@ -1,31 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   first_print.c                                      :+:      :+:    :+:   */
+/*   ls_stat.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: blee <blee@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/03/08 18:44:11 by blee              #+#    #+#             */
-/*   Updated: 2018/03/14 20:53:12 by blee             ###   ########.fr       */
+/*   Created: 2018/03/14 16:15:01 by blee              #+#    #+#             */
+/*   Updated: 2018/03/14 16:15:29 by blee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
-
-void	print_first_dir(t_btree *node, t_param *param)
-{
-	if (param->count == 1 && bt_dircheck(node))
-	{
-		//print dir
-	}
-	else 
-	{
-		//loop use ls_btinfix
-		//print all dir with the name first
-	}
-}
-
-void	print_first_files(t_btree *node, t_param *param)
-{
-
-}
